ef3f356bf42a20e9c046502884d8a51ddfe7c6b9
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-09-05 12:39:32 +0530

    Added intents for twilio

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const Polly = new AWS.Polly({
 });
 
 var conversation = watson.conversation({
-  username: "b19b4d17-6a70-44cf-b478-02abb5adc137",
-  password: "BwLrFGdany0T",
+  username: "3132dae2-65a7-4355-b2a8-9aa7d75b5021",
+  password: "0FFcPwapKeYB",
   version: "v1",
   version_date: "2017-04-21"
 });
-var conv_workspace_id = "270132c6-0104-4509-a64f-a21ef33fb47f";
+var conv_workspace_id = "3fc1cda3-2523-45c8-9804-bff797b6f959";
 
 var collife_faq_url = 'https://www.coloniallife.com/FAQ.aspx';
 
@@ -148,16 +148,8 @@ app.post('/twilio', function(req, res) {
                     if (intents_length > 0) {
                         var intents = resp.result.intents[0].intent;
                         console.log("Intents Twilio: " + resp.result.intents[0].intent);
-                        if (intents == "hometemp") {
-                            url = prop_url;
-                        } else if (intents == "carrepair" || intents == "carrent" || intents == "claimrequest") {
-                            url = auto_url;
-                        } else if (intents == "cs-contact") {
-                            url = contacts_url;
-                        } else if (intents == "billing_mail" || intents == "billing_paymentaddress" || intents == "billing_payelectronic" || intents == "billing_autopaysetup" || intents == "billing_quickpay" || intents == "billing_autopay" || intents == "billing_onetimepay" || intents == "billing_accountverify" || intents == "billing_anotheracc" || intents == "billing_paymentupdate" || intents == "billing_paymentverify" || intents == "billing_costverify") {
-                            url = billing_url;
-                        } else if (intents == "policyholders_policypaper" || intents == "policyholders_online" || intents == "policyholders_mailingaddress" || intents == "policyholders_billingaddress" || intents == "policyholders_wildfire" || intents == "policyholders_password" || intents == "policyholders_autoid") {
-                            url = policyholders_url;
+			if (intents == "accountsetup" || intents == "claimstatus" || intents == "policystatus" || intents == "policyholders_policypaper" || intents == "enrollcoverage" || intents == "fileclaim" || intents == "billing_quickpay" || intents == "billing_paypremium") {
+                            url = collife_faq_url;
                         } else if (intents == "dialCSR") {
                             callfaq = false;
                             rslt = "Okay. We will transfer the call with our representative.";
@@ -165,18 +157,18 @@ app.post('/twilio', function(req, res) {
                                 if (err) {
                                     gather.play("https://s3.amazonaws.com/twilioplay/pollyerror.mp3");
                                 } else {
-                                	const dialcsr = new VoiceResponse();
-                                	dialcsr.play("https://s3.amazonaws.com/twilioplay/chat_collife.mp3");
-									//dialcsr.dial('317-854-5164');
-									dialcsr.dial('+919994753161');
+                                    const dialcsr = new VoiceResponse();
+                                    dialcsr.play("https://s3.amazonaws.com/twilioplay/chat_collife.mp3");
+				    //dialcsr.dial('317-854-5164');
+				    dialcsr.dial('+919994753161');
 
-		                            /*const dial = dialcsr.dial({callerId: '+919840425569'});
-					  				dial.number('+12015629180');*/
+		                   /*const dial = dialcsr.dial({callerId: '+919840425569'});
+				     dial.number('+12015629180');*/
 		
-		                            res.writeHead(200, {
-		                                'Content-Type': 'text/xml'
-		                            });
-		                            res.end(dialcsr.toString());                                    
+		                    res.writeHead(200, {
+		                          'Content-Type': 'text/xml'
+		                    });
+		                    res.end(dialcsr.toString());                                    
                                 }
                             });                            
                         } else if (intents == "cs-hello") {
@@ -214,7 +206,7 @@ app.post('/twilio', function(req, res) {
                             	if (err) {
                                     gather.play("https://s3.amazonaws.com/twilioplay/pollyerror.mp3");
                                 } else {
-                                	const endprompt = new VoiceResponse();
+                                    const endprompt = new VoiceResponse();
                                     endprompt.play("https://s3.amazonaws.com/twilioplay/chat_collife.mp3");
                                     res.writeHead(200, {
                                         'Content-Type': 'text/xml'
@@ -228,8 +220,7 @@ app.post('/twilio', function(req, res) {
                             uploadaudio(rslt, function(err, data) {
                                 if (err) {
                                     gather.play("https://s3.amazonaws.com/twilioplay/pollyerror.mp3");
-                                } else {
-                                    
+                                } else {                                    
                                     gather.play("https://s3.amazonaws.com/twilioplay/chat_collife.mp3");
                                     res.writeHead(200, {
                                         'Content-Type': 'text/xml'
@@ -316,77 +307,29 @@ function get_faq_resp(url, intent, type, done){
         $('.basicBox').filter(function(){
           var data = $(this);
           count++;
-          if (intent == "hometemp"){
-            desc = data.children().eq(5).children().eq(1).text();
-          }else if (intent == "carrepair"){
-            desc = data.children().eq(4).children().eq(1).text();
-          }else if (intent == "carrent"){
-            desc = data.children().eq(5).children().eq(1).text();
-          }else if (intent == "claimrequest"){
-            var desc0 = data.children().eq(1).children().eq(1).text() + "|";
-            for (var i = 0; i < data.children().eq(1).children().eq(2).children().length; i++);
-            {
-              var desc1 = "1. " + data.children().eq(1).children().eq(2).children().eq(0).text() + "|";
-              var desc2 = "2. " + data.children().eq(1).children().eq(2).children().eq(1).text() + "|";
-              var desc3 = "3. " + data.children().eq(1).children().eq(2).children().eq(2).text() + "|";
-              var desc4 = "4. " + data.children().eq(1).children().eq(2).children().eq(3).text() + "|";
-              var desc5 = "5. " + data.children().eq(1).children().eq(2).children().eq(4).text() + "|";
-            }
-            var desc6 = data.children().eq(1).children().eq(3).text();
-            desc = desc0 + desc1 + desc2 + desc3 + desc4 + desc5 + desc6;
-          }else if ((intent == "billing_mail" || intent == "billingmail") && count == 1){
+          if (intent == "accountsetup"){
             desc = data.children().eq(0).children().eq(1).text();
-          }else if ((intent == "billing_paymentaddress" || intent == "billingpaymentaddress") && count == 1){
-            desc = data.children().eq(2).html();
-            loadtype = "html";
-          }else if ((intent == "billing_payelectronic" || intent == "billingpayelectronic") && count == 1){
-            desc = data.children().eq(3).html();
-            loadtype = "html";
-          }else if ((intent == "billing_autopaysetup" || intent == "billingautopaysetup") && count == 1){
-            desc = data.children().eq(4).html();
-            loadtype = "html";
-          }else if ((intent == "billing_quickpay" || intent == "billingquickpay") && count == 1){
-            desc = data.children().eq(5).html();
-            loadtype = "html";
-          }else if ((intent == "billing_autopay" || intent == "billingautopay") && count == 1){
-            desc = data.children().eq(6).html();
-            loadtype = "html";
-          }else if ((intent == "billing_onetimepay" || intent == "billingonetimepay") && count == 1){
-            desc = data.children().eq(7).html();
-            loadtype = "html";
-          }else if ((intent == "billing_accountverify" || intent == "billingaccountverify") && count == 1){
-            desc = data.children().eq(9).children().eq(1).text();
-          }else if ((intent == "billing_anotheracc" || intent == "billinganotheracc") && count == 1){
-            desc = data.children().eq(10).children().eq(1).text();
-          }else if ((intent == "billing_paymentupdate" || intent == "billingpaymentupdate") && count == 1){
-            desc = data.children().eq(13).children().eq(1).text();
-          }else if ((intent == "billing_paymentverify" || intent == "billingpaymentverify") && count == 1){
-            desc = data.children().eq(14).children().eq(1).text();
-          }else if ((intent == "billing_costverify" || intent == "billingcostverify") && count == 1){
-            desc = data.children().eq(15).children().eq(1).text();
-          }else if ((intent == "policyholders_policypaper" || intent == "policyholderspolicypaper") && count == 1){
-            desc = data.children().eq(0).children().eq(1).text();
-          }else if ((intent == "policyholders_online" || intent == "policyholdersonline") && count == 1){
+          }else if (intent == "claimstatus"){
             desc = data.children().eq(1).children().eq(1).text();
-          }else if ((intent == "policyholders_mailingaddress" || intent == "policyholdersmailingaddress") && count == 1){
+          }else if (intent == "policystatus"){
             desc = data.children().eq(2).children().eq(1).text();
-          }else if ((intent == "policyholders_billingaddress" || intent == "policyholdersbillingaddress") && count == 1){
+          }else if (intent == "policyholders_policypaper"){
             desc = data.children().eq(3).children().eq(1).text();
-          }else if ((intent == "policyholders_wildfire" || intent == "policyholderswildfire") && count == 1){
-            desc = data.children().eq(5).html();
-            loadtype = "html";
-          }else if ((intent == "policyholders_password" || intent == "policyholderspassword") && count == 1){
+          }else if (intent == "enrollcoverage"){
+            desc = data.children().eq(4).children().eq(1).text();
+          }else if (intent == "fileclaim"){
             desc = data.children().eq(6).children().eq(1).text();
-          }else if ((intent == "policyholders_autoid" || intent == "policyholdersautoid") && count == 1){
-            desc = data.children().eq(7).html();
-            loadtype = "html";
-          }
-        }); //end of expandCollapse filter loop
+          }else if (intent == "billing_quickpay"){
+            desc = data.children().eq(7).children().eq(1).text();
+          }else if (intent == "billing_paypremium"){
+            desc = data.children().eq(9).children().eq(1).text();
+          }	
+        }); //end of basicBox filter loop
         count = 0;
       } //end of intent check
       //console.log("Web Scrap result: "+desc);
       if (type == "Device" && loadtype == "html"){
-        if (intent != "cscontact"){  //In cscontact intent - Question is not exist in first line, So no required to remove the first line (i.e, question) from desc
+        if (intent != "cscontact"){  //In cscontact intent - Question is not exist in first line, So not required to remove the first line (i.e, question) from desc
           var startindex = desc.indexOf("</strong>");
           desc = desc.substring(startindex);
         }
